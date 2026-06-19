5a6441adf058a7427112dd76157deca55b5f81a7
Ubuntu <ubuntu@ip-172-31-41-108.us-east-2.compute.internal>
2026-06-19 05:58:46 +0000

    Added CI/CD

--- backend/server.js
+++ backend/server.js
@@ -41,3 +41,36 @@ app.post("/employees",async(req,res)=>{
 app.listen(5000,()=>{
    console.log("Server Running");
 });
+
+const mongoose = require("mongoose");
+
+
+const app = require("./app");
+
+
+mongoose.connect(
+
+"mongodb://mongodb:27017/employeedb"
+
+)
+
+.then(()=>{
+
+
+console.log("Mongo Connected");
+
+
+app.listen(5000,()=>{
+
+console.log("Server Started");
+
+});
+
+
+})
+
+.catch(err=>{
+
+console.log(err);
+
+});
