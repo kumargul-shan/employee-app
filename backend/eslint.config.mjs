1ef2b3c6bb9fa9e02d7f8de5bd1d37b6d51c0385
Ubuntu <ubuntu@ip-172-31-41-108.us-east-2.compute.internal>
2026-06-19 07:03:02 +0000

    fixed ESLint config

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,8 +1,26 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-
+ 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+{
+files: ["/*.{js,mjs,cjs}"],
+...js.configs.recommended,
+languageOptions: {
+globals: {
+...globals.node
+}
+}
+},
+{
+files: ["test//*.js"],
+languageOptions: {
+globals: {
+describe: "readonly",
+test: "readonly",
+expect: "readonly"
+}
+}
+}
 ]);
+ 
